b3b262da5de656d2e907cd5ec17b8e233e03d46a
Eli-Smith <elijah.smith23@yahoo.com>
2019-11-13 15:47:19 -0500

    Ships and People Endpoints

--- src/app/films/films.component.ts
+++ src/app/films/films.component.ts
@@ -9,6 +9,8 @@ import { FilmsService } from '../films.service';
 export class FilmsComponent implements OnInit {
 
   films: any = [];
+  ships: any = [];
+  person: any = [];
 
   constructor(
     private filmsService: FilmsService
@@ -18,7 +20,26 @@ export class FilmsComponent implements OnInit {
     this.filmsService.getFilms()
       .subscribe( film => {
         this.films = film
+        this.films = this.films.results
         console.log(this.films)
+      })
+  }
+
+  showShips() {
+    this.filmsService.getShips()
+      .subscribe( ship => {
+        this.ships = ship
+        this.ships = this.ships.results
+        console.log(this.ships)
+      })
+  }
+
+  showPeople() {
+    this.filmsService.getPeople()
+      .subscribe( people => {
+        this.person = people
+        this.person = this.person.results
+        console.log(this.person)
       })
   }
 
