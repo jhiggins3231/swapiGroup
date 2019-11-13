6c130e4f997a28945102b882022737f8d10eeefe
James <jhiggins3231@gmail.com>
2019-11-13 14:16:21 -0500

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppComponent } from './app.component';
 import { PeopleComponent } from './people/people.component';
 import { FilmsComponent } from './films/films.component';
 import { ShipsComponent } from './ships/ships.component';
+import { SearchComponent } from './search/search.component';
+
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
     PeopleComponent,
     FilmsComponent,
-    ShipsComponent
+    ShipsComponent,
+    SearchComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
